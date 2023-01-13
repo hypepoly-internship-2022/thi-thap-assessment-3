8370905230a59d23524c7e4b9e6be8938401a815
Dangthap <dangthithap9@gmail.com>
2023-01-13 10:17:27 +0700

    fix

--- Assets/Script/CubeRotase.cs
+++ Assets/Script/CubeRotase.cs
@@ -3,15 +3,43 @@ using UnityEngine;
 
 public class CubeRotase : MonoBehaviour
 {
-    [SerializeField] private Vector3 mousePosition;
+    private Camera myCam;
+    private Vector3 screenPos;
+    private float angleObj;
+    private Collider2D col;
 
-    private void OnMouseDrag() 
+    private void Awake() 
     {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z= Camera.main.nearClipPlane + 4;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        Vector3 ojbPos = new Vector3(mousePosition.x, mousePosition.y ); 
-        transform.up = ojbPos;
+        myCam = Camera.main;
+    }
 
+    private void Start()
+    {
+        col = this.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(col == Physics2D.OverlapPoint(mousePos))
+            {
+                screenPos = myCam.WorldToScreenPoint(transform.position);
+                Vector3 vec3 = Input.mousePosition - screenPos;
+                angleObj = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+            }
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            if(col == Physics2D.OverlapPoint(mousePos))
+            {
+                Vector3 vec3 = Input.mousePosition - screenPos;
+                float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, 0, angle + angleObj);
+            }
+        }
     }
 }
